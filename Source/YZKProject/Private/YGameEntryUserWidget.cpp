ba031380a36c70bbcab8997e35501ba367778dbc
tianlanqqcom <1244796890@qq.com>
2023-10-27 23:01:14 +0800

    大厅联网功能修复完成

--- Source/YZKProject/Private/YGameEntryUserWidget.cpp
+++ Source/YZKProject/Private/YGameEntryUserWidget.cpp
@@ -5,34 +5,19 @@
 
 #include <iostream>
 #include <string>
-
-#if PLATFORM_WINDOWS
-#include <ws2tcpip.h>
-#include <WinSock2.h>
-#pragma comment(lib, "ws2_32.lib")
-typedef long long ssize_t;
-#endif
-
-#if PLATFORM_LINUX || PLATFORM_ANDROID
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#endif
+#include <Kismet/GameplayStatics.h>
 
 
-
-int32 UYGameEntryUserWidget::CreateRoom()
+int UYGameEntryUserWidget::CreateClientSocket()
 {
-    UE_LOG(LogTemp, Warning, TEXT("Call CreateRoom"));
-    #if PLATFORM_WINDOWS
+#if PLATFORM_WINDOWS
+
+    // Init WSA
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
-        // std::cerr << "Failed to initialize Winsock" << std::endl;
-        UE_LOG(LogTemp, Warning, L"CreateRoom: Failed to initialize Winsock");
         WSACleanup();
-        return 0;
+        return -1;
     }
 
 #endif
@@ -42,306 +27,205 @@ int32 UYGameEntryUserWidget::CreateRoom()
     int clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
     if (clientSocket < 0)
     {
-        // std::cerr << "无法创建客户端socket！" << std::endl;
-        UE_LOG(LogTemp, Warning, L"CreateRoom: Failed to create client socket");
-        // WSACleanup();
-        return 0;
+        return -1;
     }
 
-#if PLATFORM_WINDOWS
-    DWORD timeout = 3000;
-    if (setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout)) == SOCKET_ERROR)
-    {
-        std::cerr << "Failed to set socket timeout" << std::endl;
-        closesocket(clientSocket);
-        WSACleanup();
-        return 0;
-    }
-#endif
+    return clientSocket;
+}
 
-#if PLATFORM_LINUX || PLATFORM_ANDROID
-    // 设置超时时间为2秒
-    timeval timeout;
-    timeout.tv_sec = 3;
-    timeout.tv_usec = 0;
-
-    // 设置套接字选项
-    if (setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout)) == -1)
-    {
-        // std::cerr << "无法设置套接字选项" << std::endl;
+void UYGameEntryUserWidget::CloseSocket(int SocketFd)
+{
 #if PLATFORM_WINDOWS
-        closesocket(clientSocket);
-        WSACleanup();
+    closesocket(SocketFd);
+    WSACleanup();
 #elif PLATFORM_LINUX || PLATFORM_ANDROID
-        close(clientSocket);
+    close(SocketFd);
 #endif
-        return 0;
+}
+
+int UYGameEntryUserWidget::SetSocketTimeout(int SocketFd, int Time_s, int Time_ms)
+{
+#if PLATFORM_WINDOWS
+    DWORD timeout = Time_s * 1000 + Time_ms;
+    if (setsockopt(SocketFd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout)) == -1)
+    {
+        CloseSocket(SocketFd);
+        return -1;
+    }
+
+#elif PLATFORM_LINUX || PLATFORM_ANDROID
+    timeval timeout;
+    timeout.tv_sec = Time_s;
+    timeout.tv_usec = Time_ms;
+
+    if (setsockopt(SocketFd, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout)) == -1)
+    {
+        CloseSocket(SocketFd);
+        return -1;
     }
 #endif
+    return 0;
+}
 
-
-    // 设置服务器地址和端口
-    sockaddr_in serverAddr{};
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(7776);
-    inet_pton(AF_INET, "62.234.13.91", &(serverAddr.sin_addr));
-
-    // 发送数据给服务器
-    std::string requestData = "Create";
-    ssize_t bytesSent = sendto(clientSocket, requestData.c_str(), requestData.size(), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+int UYGameEntryUserWidget::SendDataToServer(const char* Message, int ClientFd, sockaddr_in* ServerAddr, int ServerAddrLen)
+{
+    int bytesSent = sendto(ClientFd, Message, strlen(Message), 0, (struct sockaddr*)ServerAddr, ServerAddrLen);
     if (bytesSent < 0)
     {
-        // std::cerr << "发送数据失败！" << std::endl;
-#if PLATFORM_WINDOWS
-        closesocket(clientSocket);
-        WSACleanup();
-#elif PLATFORM_LINUX || PLATFORM_ANDROID
-        close(clientSocket);
-#endif
-        return 0;
+        CloseSocket(ClientFd);
+        return -1;
     }
-    UE_LOG(LogTemp, Warning, TEXT("CreateRoom: Finish send."));
-    // 接收服务器返回的数据
-    char buffer[1024] = {};
-    sockaddr_in serverResponseAddr{};
-    socklen_t serverResponseAddrSize = sizeof(serverResponseAddr);
+    return 0;
+}
 
+int UYGameEntryUserWidget::RecvFromServer(char* MessageBuffer, int BufferSize, int ClientFd, sockaddr_in* ServerResponseAddr,
+    socklen_t* ServerResponseAddrLen)
+{
 #if PLATFORM_WINDOWS
-    int bytesReceived = recvfrom(clientSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&serverResponseAddr, &serverResponseAddrSize);
+    int bytesReceived = recvfrom(ClientFd, MessageBuffer, BufferSize, 0, (struct sockaddr*)ServerResponseAddr,
+        ServerResponseAddrLen);
     if (bytesReceived == SOCKET_ERROR)
     {
         if (WSAGetLastError() == WSAETIMEDOUT)
         {
-            UE_LOG(LogTemp, Warning, TEXT("CreateRoom: Finish send.Timeout"));
-            // std::cerr << "Timeout occurred" << std::endl;
-        }
-        else
-        {
-            UE_LOG(LogTemp, Warning, TEXT("CreateRoom: Finish send.Other Error"));
-            // std::cerr << "Failed to receive data" << std::endl;
+            UE_LOG(LogTemp, Warning, L"RecvFromServer:Timeout.");
+            CloseSocket(ClientFd);
+            return -1; // Timeout
         }
 
-#if PLATFORM_WINDOWS
-        closesocket(clientSocket);
-        WSACleanup();
-#elif PLATFORM_LINUX || PLATFORM_ANDROID
-        close(clientSocket);
-#endif
-        return 0;
+        UE_LOG(LogTemp, Warning, L"RecvFromServer:Other Error = %d", WSAGetLastError());
+    	CloseSocket(ClientFd);
+    	return -2; // Other Error
     }
-    
+
 #elif PLATFORM_LINUX || PLATFORM_ANDROID
 
-    ssize_t bytesReceived = recvfrom(clientSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&serverResponseAddr, &serverResponseAddrSize);
+    ssize_t bytesReceived = recvfrom(ClientFd, MessageBuffer, BufferSize, 0, (struct sockaddr*)ServerResponseAddr,
+        (socklen_t*)ServerResponseAddrLen);
     if (bytesReceived < 0)
     {
-        UE_LOG(LogTemp, Warning, TEXT("CreateRoom: Finish recv, but err: %lld."), bytesReceived);
-
         if (errno == EAGAIN || errno == EWOULDBLOCK)
         {
-            UE_LOG(LogTemp, Warning, TEXT("CreateRoom: timeout"));
-    }
+            CloseSocket(ClientFd);
+            return -1;
+        }
         else
         {
-            UE_LOG(LogTemp, Warning, TEXT("CreateRoom: Recv failed.errno = %d"), errno);
-#if PLATFORM_WINDOWS
-            UE_LOG(LogTemp, Warning, TEXT("CreateRoom: Win wsa err = %d"), WSAGetLastError());
-#endif
-
+            CloseSocket(ClientFd);
+            return -2;
         }
+    }
 #endif
+    return 0;
+}
 
+int32 UYGameEntryUserWidget::CreateRoom()
+{
+    int ClientSocket = CreateClientSocket();
+    if (ClientSocket < 0)
+    {
+        return 0;
+    }
 
-    
+    if (SetSocketTimeout(ClientSocket, 3))
+    {
+        return 0;
+    }
 
-    // 处理接收到的数据
-    // std::string responseData(buffer, bytesReceived);
-    // std::cout << "接收到的数据: " << responseData << std::endl;
+    char Message[] = "Create";
+    char RecvBuffer[1024] = {};
 
-    // 关闭客户端socket
-#if PLATFORM_WINDOWS
-    closesocket(clientSocket);
-    WSACleanup();
-#elif PLATFORM_LINUX || PLATFORM_ANDROID
-    close(clientSocket);
-#endif
+    sockaddr_in ServerAddr{};
+    ServerAddr.sin_family = AF_INET;
+    ServerAddr.sin_port = htons(7776);
+    inet_pton(AF_INET, "62.234.13.91", &(ServerAddr.sin_addr));
 
+    if (SendDataToServer(Message, ClientSocket, &ServerAddr, sizeof(ServerAddr)))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("CreateRoom: Send Failed."));
+        return 0;
+    }
+
+    sockaddr_in ServerResponseAddr{};
+    socklen_t ServerResponseAddrSize = sizeof(ServerResponseAddr);
+
+    if (RecvFromServer(RecvBuffer, sizeof(RecvBuffer), ClientSocket, &ServerResponseAddr, &ServerResponseAddrSize))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("CreateRoom: Recv Failed."));
+        return 0;
+    }
+
+    CloseSocket(ClientSocket);
 
     char* EndPtr;
-    return strtol(buffer, &EndPtr, 10);
+    return strtol(RecvBuffer, &EndPtr, 10);
 }
 
 TArray<int32> UYGameEntryUserWidget::FindRooms()
 {
-    UE_LOG(LogTemp, Warning, TEXT("Call FindRooms"));
-    TArray<int32> Result;
+	UE_LOG(LogTemp, Warning, TEXT("Call FindRooms"));
+	TArray<int32> Result;
 
-#if PLATFORM_WINDOWS
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-    {
-        // std::cerr << "Failed to initialize Winsock" << std::endl;
-        UE_LOG(LogTemp, Warning, L"FindRooms: Failed to initialize Winsock");
-        WSACleanup();
-        return Result;
-    }
+	int ClientSocket = CreateClientSocket();
+	if (ClientSocket < 0)
+	{
+		return Result;
+	}
 
-#endif
+	if (SetSocketTimeout(ClientSocket, 3))
+	{
+		return Result;
+	}
 
+	char Message[] = "Find";
+	char RecvBuffer[1024] = {};
 
-    // 创建客户端socket
-    int clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (clientSocket < 0)
-    {
-        // std::cerr << "无法创建客户端socket！" << std::endl;
-        UE_LOG(LogTemp, Warning, L"FindRooms: Failed to create client socket");
-        // WSACleanup();
-        return Result;
-    }
+	sockaddr_in ServerAddr{};
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_port = htons(7776);
+	inet_pton(AF_INET, "62.234.13.91", &(ServerAddr.sin_addr));
 
-#if PLATFORM_WINDOWS
-    DWORD timeout = 3000;
-    if (setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout)) == SOCKET_ERROR)
-    {
-        std::cerr << "Failed to set socket timeout" << std::endl;
-        closesocket(clientSocket);
-        WSACleanup();
-        return Result;
-    }
-#endif
+	if (SendDataToServer(Message, ClientSocket, &ServerAddr, sizeof(ServerAddr)))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("FindRooms: Send Failed."));
+		return Result;
+	}
 
-#if PLATFORM_LINUX || PLATFORM_ANDROID
-    // 设置超时时间为2秒
-    timeval timeout;
-    timeout.tv_sec = 3;
-    timeout.tv_usec = 0;
+	sockaddr_in ServerResponseAddr{};
+	socklen_t ServerResponseAddrSize = sizeof(ServerResponseAddr);
 
-    // 设置套接字选项
-    if (setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout)) == -1)
-    {
-        // std::cerr << "无法设置套接字选项" << std::endl;
-#if PLATFORM_WINDOWS
-        closesocket(clientSocket);
-        WSACleanup();
-#elif PLATFORM_LINUX || PLATFORM_ANDROID
-        close(clientSocket);
-#endif
-        return Result;
-    }
-#endif
+	if (RecvFromServer(RecvBuffer, sizeof(RecvBuffer), ClientSocket, &ServerResponseAddr, &ServerResponseAddrSize))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("FindRooms: Recv Failed."));
+		return Result;
+	}
+	CloseSocket(ClientSocket);
 
+	UE_LOG(LogTemp, Warning, TEXT("Call FindRooms: Finish recv, begin process."));
+	const int32* ResultArray = reinterpret_cast<int32*>(RecvBuffer);
 
-    // 设置服务器地址和端口
-    sockaddr_in serverAddr{};
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(7776);
-    inet_pton(AF_INET, "62.234.13.91", &(serverAddr.sin_addr));
+	const int32 ResultLength = ResultArray[0];
+	UE_LOG(LogTemp, Warning, TEXT("Call FindRooms: ArrayLength = %d"), ResultLength);
 
-    // 发送数据给服务器
-    std::string requestData = "Find";
-    ssize_t bytesSent = sendto(clientSocket, requestData.c_str(), requestData.size(), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-    if (bytesSent < 0)
-    {
-        // std::cerr << "发送数据失败！" << std::endl;
-#if PLATFORM_WINDOWS
-        closesocket(clientSocket);
-        WSACleanup();
-#elif PLATFORM_LINUX || PLATFORM_ANDROID
-        close(clientSocket);
-#endif
-        return Result;
-    }
-    UE_LOG(LogTemp, Warning, TEXT("FindRooms: Finish send."));
-    // 接收服务器返回的数据
-    char buffer[1024] = {};
-    sockaddr_in serverResponseAddr{};
-    socklen_t serverResponseAddrSize = sizeof(serverResponseAddr);
+	for (int32 i = 0; i < ResultLength; i++)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Call FindRooms: Array[%d] = %d"), i, ResultArray[i + 1]);
+		Result.Add(ResultArray[i + 1]);
 
-#if PLATFORM_WINDOWS
-    int bytesReceived = recvfrom(clientSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&serverResponseAddr, &serverResponseAddrSize);
-    if (bytesReceived == SOCKET_ERROR)
-    {
-        if (WSAGetLastError() == WSAETIMEDOUT)
-        {
-            UE_LOG(LogTemp, Warning, TEXT("FindRooms: Finish send.Timeout"));
-            // std::cerr << "Timeout occurred" << std::endl;
-        }
-        else
-        {
-            UE_LOG(LogTemp, Warning, TEXT("FindRooms: Finish send.Other Error"));
-            // std::cerr << "Failed to receive data" << std::endl;
-        }
-
-#if PLATFORM_WINDOWS
-        closesocket(clientSocket);
-        WSACleanup();
-#elif PLATFORM_LINUX || PLATFORM_ANDROID
-        close(clientSocket);
-#endif
-        return Result;
-    }
-    
-#elif PLATFORM_LINUX || PLATFORM_ANDROID
-
-    ssize_t bytesReceived = recvfrom(clientSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&serverResponseAddr, &serverResponseAddrSize);
-    if (bytesReceived < 0)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("FindRooms: Finish recv, but err: %lld."), bytesReceived);
-
-        if (errno == EAGAIN || errno == EWOULDBLOCK)
-        {
-            UE_LOG(LogTemp, Warning, TEXT("FindRooms: timeout"));
-    }
-        else
-        {
-            UE_LOG(LogTemp, Warning, TEXT("FindRooms: Recv failed.errno = %d"), errno);
-#if PLATFORM_WINDOWS
-            UE_LOG(LogTemp, Warning, TEXT("FindRooms: Win wsa err = %d"), WSAGetLastError());
-#endif
-
-        }
-#endif
-
-
-    
-
-    // 处理接收到的数据
-    // std::string responseData(buffer, bytesReceived);
-    // std::cout << "接收到的数据: " << responseData << std::endl;
-
-    // 关闭客户端socket
-#if PLATFORM_WINDOWS
-    closesocket(clientSocket);
-    WSACleanup();
-#elif PLATFORM_LINUX || PLATFORM_ANDROID
-    close(clientSocket);
-#endif
-
-    UE_LOG(LogTemp, Warning, TEXT("Call FindRooms: Finish recv, begin process."));
-    const int32* ResultArray = reinterpret_cast<int32*>(buffer);
-
-    const int32 ResultLength = ResultArray[0];
-    UE_LOG(LogTemp, Warning, TEXT("Call FindRooms: ArrayLength = %d"), ResultLength);
-
-    for(int32 i = 0;i<ResultLength;i++)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("Call FindRooms: Array[%d] = %d"), i, ResultArray[i + 1]);
-        Result.Add(ResultArray[i + 1]);
-
-        if(i == 255)
-        {
-            break;
-        }
-    }
-    return Result;
+		if (i == 255)
+		{
+			break;
+		}
+	}
+	return Result;
 
 }
 
 FString UYGameEntryUserWidget::JoinRoom(int32 TargetPort)
 {
-    if(TargetPort > 0 && TargetPort <= 65535)
+    if (TargetPort > 0 && TargetPort <= 65535)
     {
-        return FString::Printf(L"62.234.13.91:%d", TargetPort);
+        return FString::Printf(TEXT("62.234.13.91:%d"), TargetPort);
     }
     return FString();
 }
--- Source/YZKProject/Public/YGameEntryUserWidget.h
+++ Source/YZKProject/Public/YGameEntryUserWidget.h
@@ -4,6 +4,21 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+
+#if PLATFORM_WINDOWS
+#include <ws2tcpip.h>
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
+typedef long long ssize_t;
+#endif
+
+#if PLATFORM_LINUX || PLATFORM_ANDROID
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+
 #include "YGameEntryUserWidget.generated.h"
 
 /**
@@ -23,5 +38,17 @@ public:
 
 	UFUNCTION(BlueprintCallable)
 	FString JoinRoom(int32 TargetPort);
+
+private:
+	// Net Functions
+	static int CreateClientSocket();
+
+	static void CloseSocket(int SocketFd);
+
+	static int SetSocketTimeout(int SocketFd, int Time_s, int Time_ms = 0);
+
+	static int SendDataToServer(const char* Message, int ClientFd, struct sockaddr_in* ServerAddr, int ServerAddrLen);
+
+	static int RecvFromServer(char* MessageBuffer, int BufferSize, int ClientFd, sockaddr_in* ServerResponseAddr, socklen_t* ServerResponseAddrLen);
 	
 };
